b1b1b71b71cbf65c38549b5f0f11b61b8daa0c8f
VenSnow <timhale2104@icloud.com>
2022-10-28 16:44:47 +0500

    Lesson 1, 1 task completed

---
+++ lesson1/task_1.cpp
@@ -0,0 +1,19 @@
+//
+/*
+ * Напишите программу на C++, которая отобразит ваше имя и адрес (можете
+указать фиктивные данные).
+ */
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    string fullName = "Tim Hale";
+    string address = "Tashkent, Uzbekistan";
+    cout << "My name is " << fullName << endl;
+    cout << "I live in " << address << endl;
+
+    return 0;
+}
